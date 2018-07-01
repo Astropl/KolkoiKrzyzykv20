8e539516def60fa3cef7e5e11950f7b400db5279
Pawel <martys.pawel@gmail.com>
2018-07-01 23:26:21 +0200

    Doaodano obsługe watku na masterze

--- src/main/java/SDA/Time.java
+++ src/main/java/SDA/Time.java
@@ -13,23 +13,23 @@ public class Time implements Runnable {
         System.out.println(czas);
 
         breakFor5Seconds();
-
+run();
     }
 
     public void breakFor5Seconds() {
         System.out.println("Przerwa kilka sekund");
-        //for (int i = 2; i >= 0; i--) {
-        for (;;)
-        {
-            //System.out.println(i);
+        for (int i = 1; i >= 0; i--) {
+            //for (;;)
+            {
+                //System.out.println(i);
 
-            System.out.println(LocalDateTime.now());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                System.out.println(LocalDateTime.now());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+
             }
-
         }
-    }
-}
+    }}
 
